14eabe96399cb9112f09eb5aad32a689cb552b64
xuanqis <1174223849@qq.com>
2019-07-06 16:58:20 +0800

    和风天气报invalid key

---
+++ FirstCoding/hefeng/src/main/java/com/example/hefeng/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.hefeng;
+
+import android.app.Application;
+import android.util.Log;
+
+import interfaces.heweather.com.interfacesmodule.view.HeConfig;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("xuanqis", "初始化");
+        HeConfig.init("HE1907061632271763", "b28234a549bb4dffaa437844402cc85f");
+        HeConfig.switchToFreeServerNode();
+    }
+}
